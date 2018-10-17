67daa568981a2551e6b0b357da562f07360fc8ba
lhyuan <1028417@qq.com>
2018-10-18 05:33:52 +0800

    容器汇总

--- jstl/define.h
+++ jstl/define.h
@@ -8,23 +8,26 @@
 
 using namespace std;
 
-typedef size_t TD_PosType;
-typedef size_t TD_SizeType;
+namespace NS_JSTL
+{
+	typedef size_t TD_PosType;
+	typedef size_t TD_SizeType;
 
-template <typename T>
-using InitList_T = const initializer_list<T>&;
+	template <typename T>
+	using InitList_T = const initializer_list<T>&;
 
-template <typename T, typename RET>
-using CB_T_Ret = const function<RET(const T&v)>&;
+	template <typename T, typename RET>
+	using CB_T_Ret = const function<RET(const T&v)>&;
 
-template <typename T>
-using CB_T_void = CB_T_Ret<T, void>;
+	template <typename T>
+	using CB_T_void = CB_T_Ret<T, void>;
 
-template <typename T>
-using CB_T_bool = CB_T_Ret<T, bool>;
+	template <typename T>
+	using CB_T_bool = CB_T_Ret<T, bool>;
 
-template <typename T>
-using CB_T_Pos = const function<bool(T, TD_PosType)>&;
+	template <typename T>
+	using CB_T_Pos = const function<bool(T, TD_PosType)>&;
+}
 
 #define __SuperType(T) typename __Super::T
 #define __UsingSuperType(T) using T = __SuperType(T)
---
+++ jstl/jstl.h
@@ -0,0 +1,10 @@
+
+#ifndef __jstl_h
+#define __jstl_h
+
+#include "JSArray.h"
+#include "PtrArray.h"
+#include "JSSet.h"
+#include "JSMap.h"
+
+#endif // __jstl_h
